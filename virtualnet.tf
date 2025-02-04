48437fb9fa72221b92fba10642a9fa328b99fa92
Yvi-compt <yvesplacide@gmail.com>
2025-02-03 20:05:30 -0500

    Create virtualnet.tf

---
+++ virtualnet.tf
@@ -0,0 +1,58 @@
+resource "azurerm_resource_group" "vmlinuxrg" {
+  name     = "virtual-m-linux-resources"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnetwork"
+  address_space       = ["192.168.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "subneta" {
+  name                 = "subnet-a"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["192.168.2.0/24"]
+}
+
+resource "azurerm_network_interface" "netinterface" {
+  name                = "net-interface-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "subnet-a"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vmlinux" {
+  name                = "virtual-machine-linux"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
