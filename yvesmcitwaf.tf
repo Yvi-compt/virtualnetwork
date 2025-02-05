562e6178796c3b9a6e7629b02e7ba50ca873828a
Yvi-compt <yvesplacide@gmail.com>
2025-02-05 18:37:56 -0500

    Update yvesmcitwaf.tf

--- yvesmcitwaf.tf
+++ yvesmcitwaf.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "vmlinuxrg" {
+resource "azurerm_resource_group" "yvesmcitrg" {
   name     = "yves-waf-resource-group"
   location = "Eastus"
 }
 
 resource "azurerm_application_gateway" "yvesappgw" {
   name                = "yvesAppGateway"
-  location            = azurerm_resource_group.vmlinuxrg.location
-  resource_group_name = azurerm_resource_group.vmlinuxrg.name
+  location            = azurerm_resource_group.yvesmcitrg.location
+  resource_group_name = azurerm_resource_group.yvesmcitrg.name
 
   sku {
     name     = "WAF_v2"
@@ -16,7 +16,7 @@ resource "azurerm_application_gateway" "yvesappgw" {
 
   gateway_ip_configuration {
     name      = "my-gateway-ip-config"
-    subnet_id = azurerm_subnet.subneta.id
+    subnet_id = azurerm_subnet.subnetb.id
   }
 
   frontend_ip_configuration {
@@ -72,24 +72,24 @@ resource "azurerm_public_ip" "appgw_pip" {
   sku                 = "Standard"
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnetb" {
   name                = "waf-vnet"
   location            = azurerm_resource_group.vmlinuxrg.location
   resource_group_name = azurerm_resource_group.vmlinuxrg.name
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "subneta" {
-  name                 = "waf-subnet"
-  resource_group_name  = azurerm_resource_group.vmlinuxrg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "subnetb" {
+  name                 = "yvesmcit-waf-subnet"
+  resource_group_name  = azurerm_resource_group.yvesmcitrg.name
+  virtual_network_name = azurerm_virtual_network.vnetb.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_web_application_firewall_policy" "waf_policy" {
   name                = "yvesWAFPolicy"
-  resource_group_name = azurerm_resource_group.vmlinuxrg.name
-  location            = azurerm_resource_group.vmlinuxrg.location
+  resource_group_name = azurerm_resource_group.yvesmcitrg.name
+  location            = azurerm_resource_group.yvesmcitrg.location
 
   policy_settings {
     enabled                     = true
