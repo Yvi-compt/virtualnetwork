acd2417e8e2b2bf3b93a09b8f24017df2a61f564
Yvi-compt <yvesplacide@gmail.com>
2025-02-04 19:29:57 -0500

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -6,37 +6,37 @@ resource "azurerm_resource_group" "vmlinuxrg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnetwork"
   address_space       = ["192.168.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.vmlinuxrg.location
+  resource_group_name = azurerm_resource_group.vmlinuxrg.name
 }
 
 resource "azurerm_subnet" "subneta" {
   name                 = "subnet-a"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.vmlinuxrg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.2.0/24"]
 }
 
 resource "azurerm_network_interface" "netinterface" {
   name                = "net-interface-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.vmlinuxrg.location
+  resource_group_name = azurerm_resource_group.vmlinuxrg.name
 
   ip_configuration {
     name                          = "subnet-a"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.subneta.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vmlinux" {
   name                = "virtual-machine-linux"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.vmlinuxrg.name
+  location            = azurerm_resource_group.vmlinuxrg.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.netinterface.id,
   ]
 
   admin_ssh_key {
