820555953114cd0a7814a0bac5b211bcbc3a5073
Yvi-compt <yvesplacide@gmail.com>
2025-02-05 19:56:56 -0500

    Update yvesmcitfunction.tf

--- yvesmcitfunction.tf
+++ yvesmcitfunction.tf
@@ -28,7 +28,7 @@ resource "azurerm_linux_function_app" "yvesmcitfunction" {
   name                       = "test-azure-functions"
   location                   = azurerm_resource_group.yvesmcitrg.location
   resource_group_name        = azurerm_resource_group.yvesmcitrg.name
-  app_service_plan_id        = azurerm_service_plan.yvesmcitplan.id
+  service_plan_id            = azurerm_service_plan.yvesmcitplan.id
   storage_account_name       = azurerm_storage_account.yvesmcitstorage.name
   storage_account_access_key = azurerm_storage_account.yvesmcitstorage.primary_access_key
 }
