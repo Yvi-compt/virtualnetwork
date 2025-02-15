783f3d3c2e438bc6a63cd1d4bf841fd57cd0d200
Yvi-compt <yvesplacide@gmail.com>
2025-02-14 20:18:51 -0500

    Create mcitout.tf

---
+++ mcitout.tf
@@ -0,0 +1,12 @@
+locals {
+Students = ["Yves", "Maryam", "Damita", "Bachir", "Braeden", ""Benito", "Joseph", "Arnault"]
+ Teachers = ["German Torres", "Keyvan Keymanesh", "Aldo Andrade","Sami Islam"]
+}
+
+# Generate a unique list of Student-Teacher combinations
+output "app_McitStudentTeacher_combos" {
+  value = tolist(toset([
+    for stud in local.Students : 
+    [for teach in local.apps : "${stud}-${teach}"]
+  ]))
+}
