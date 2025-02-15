e2836294f873d46d6121002d8da4fa3e2e116bcd
Yvi-compt <yvesplacide@gmail.com>
2025-02-14 19:40:00 -0500

    Update yvioutput.tf

--- yvioutput.tf
+++ yvioutput.tf
@@ -1,9 +1,9 @@
 output "winterlistOfSport_name" {
-value       = { for idx, name in local.winterlistOfSports : idx => "Sport Name: ${name}" \n}
+value       = { for winterlistOfSport name in local.winterlistOfSports : winterlistOfSport => "Sport Name: ${name}" \n}
 }
 output "total_output_name" {
-   value = { for idx, name in local.total_output: idx => "total out: ${name}" \n }
+   value = { for total_output name in local.total_output: total_output => "total out: ${name}" \n }
 }
 output "characters_name" {
-value =   { for idx, name in local.characters: idx => "character name: ${name}" \n}
+value =   { for characters name in local.characters: characters => "character name: ${name}" \n}
 }
